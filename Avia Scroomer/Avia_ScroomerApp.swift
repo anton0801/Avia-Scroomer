be79e3a0910cf4d4a70803097ff2f44fce4fea96
Anton <danilovanton724@gmail.com>
2024-05-09 22:51:58 +0200

    Initial Commit

---
+++ Avia Scroomer/Avia_ScroomerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Avia_ScroomerApp.swift
+//  Avia Scroomer
+//
+//  Created by Anton on 9/5/24.
+//
+
+import SwiftUI
+
+@main
+struct Avia_ScroomerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
